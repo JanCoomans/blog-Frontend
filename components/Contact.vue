37a3bec207d76ae1c9112abd8675004c63b95aa7
Jan Coomans <jan.coomans@student.pxl.be>
2023-01-23 15:34:23 +0100

    Added error page.
    Custom error page added. There is however no error handeling in normal website code. ( yet )

--- components/Contact.vue
+++ components/Contact.vue
@@ -7,9 +7,8 @@
             <div class="max-w-3xl mx-auto flex flex-wrap justify-center gap-8">
                 <div class="contact-item" v-for="link in data.attributes.tag_links.data" :key="link.id">
                     <div class="w-6 h-6 mr-4">
-                        <img
-                        :src="`${fileserver}${link.attributes.icon.data.attributes.url}`"
-                        :alt="link.attributes.icon.data.attributes.alternativeText">
+                        <img :src="`${fileserver}${link.attributes.icon.data.attributes.url}`"
+                            :alt="link.attributes.icon.data.attributes.alternativeText">
                     </div>
                     <div class="contact-divide-line"></div>
                     <h4>{{ link.attributes.tag }}</h4>
@@ -17,9 +16,9 @@
             </div>
 
             <div class="max-w-xs w-full mx-auto flex justify-around mt-16">
-                <NuxtLink class="contact-social" v-for="link in data.attributes.links.data" :key="link.id" :to="link.attributes.URL" target="_blank">
-                    <img
-                        :src="`${fileserver}${link.attributes.icon.data.attributes.url}`"
+                <NuxtLink class="contact-social" v-for="link in data.attributes.links.data" :key="link.id"
+                    :to="link.attributes.URL" target="_blank">
+                    <img :src="`${fileserver}${link.attributes.icon.data.attributes.url}`"
                         :alt="link.attributes.icon.data.attributes.alternativeText">
                 </NuxtLink>
             </div>
@@ -29,9 +28,14 @@
 </template>
 
 <script setup>
-const { find } = useStrapi();
-const { data } = await find('contact?populate=deep');
-const { fileserver } = useRuntimeConfig();
+    const { find } = useStrapi();
+    const { data } = await find('contact?populate=deep');
+    const { fileserver } = useRuntimeConfig();
+    /*
+    if (!data.value) {
+        throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+    }
+    */
 </script>
 
 <style lang="scss" scoped>
---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center bg-primary-900 p-7 text-slate-100">
+        <div class="bg-secondary rounded-md shadow-xl-dark text-center p-7 h-80 aspect-square inline-flex flex-col items-center justify-center">
+            <p class="text-5xl font-bold">Error: {{ error.statusCode }}</p>
+            <p class="mt-4">{{ error.message }}</p>
+            <button class="btn btn-accent mt-7 py-2 px-4" @click="handleClearError">Go home ...</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['error'])
+
+    const handleClearError = () => clearError({ redirect: '/' })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
